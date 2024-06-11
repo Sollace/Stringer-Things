ea70b4c0b858dcb3fccfa945f1e768430d3f40ed
Sollace <sollacea@gmail.com>
2024-06-11 15:24:03 +0100

    Fixed string boots not rendering on the player

--- src/main/java/com/sollace/stringerthings/StringArmorMaterial.java
+++ src/main/java/com/sollace/stringerthings/StringArmorMaterial.java
@@ -9,15 +9,14 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Identifier;
 
 public class StringArmorMaterial {
     public static final RegistryEntry<ArmorMaterial> INSTANCE = Registry.registerReference(
-            Registries.ARMOR_MATERIAL, new Identifier("stringer_things", "string"), new ArmorMaterial(
+            Registries.ARMOR_MATERIAL, StringerThings.id("string"), new ArmorMaterial(
             new EnumMap<>(ArmorItem.Type.class),
             9000,
             SoundEvents.ITEM_ARMOR_EQUIP_LEATHER,
             StringToolMaterial.INSTANCE::getRepairIngredient,
-            List.<ArmorMaterial.Layer>of(),
+            List.of(new ArmorMaterial.Layer(StringerThings.id("string"))),
             0F, 0F));
 }
